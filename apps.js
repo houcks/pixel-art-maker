6c7e68c1de1b80830e829554e036aeb6d1e6d0b8
Shaun Houck <shaun@Shauns-MBP.attlocal.net>
2022-02-06 22:29:26 -0700

    Expanded grid and color palette. Added comments. Added ability to paint multiple colors.

--- apps.js
+++ apps.js
@@ -1,37 +1,49 @@
+var brushColor = 'black';
 
-let grid = document.createElement('div');
-grid.classList.add('grid');
+let colors = ['red', 'darkred', 'crimson', 'darkorange', 'orange', 'gold', 
+            'lightyellow', 'khaki', 'yellow', 'lawngreen', 'green', 'darkgreen', 
+            'blue', 'mediumblue', 'darkblue', 'darkviolet', 'purple', 'indigo', 'pink', 
+            'hotpink', 'deeppink', 'white', 'gray', 'black'];
+
+let grid = document.getElementById('grid');
 grid.style.cssText = "height: 100%; width: 100%;"
-document.body.appendChild(grid);
 
-
+//removes old color if not matching current brush color or paints new color
 function toggleColor(event){
-    event.target.classList.toggle('red');   
-}
-
-function createGrid(colLength, rowLength){
-    for(var i = 0; i < rowLength; i++){
-        let row = document.createElement('div');
-        row.classList.add('row');
-        grid.appendChild(row);
-        for(var j = 0; j < colLength; j++){
-            let square = document.createElement('div');
-            square.classList.add('column');
-            square.addEventListener('click', toggleColor);
-            row.appendChild(square);
-        }
+    if(event.target.classList.item(1) !== brushColor){
+        event.target.classList.remove(event.target.classList.item(1));
     }
+    event.target.classList.toggle(`${brushColor}`);   
 }
 
+function createGrid(size){
+        for(var j = 0; j < size; j++){
+            let squares = document.createElement('div');
+            squares.classList.add('square');
+            squares.addEventListener('click', toggleColor);
+            grid.appendChild(squares);
+        }
+}
+
+//loops over array of colors to create palette of colors to choose from
 function createPalette(){
     let footer = document.querySelector('footer');
-    let colors = ['red', 'blue'];
-    for(var i = 0; i < 2; i++){
+    for(var i = 0; i < colors.length; i++){
         let colorSquare = document.createElement('div');
-        colorSquare.classList.add('palette');
-        //colorSquare.style.classList.toggle(`${colors[i]}`)
+        colorSquare.classList.toggle('palette');
+        colorSquare.classList.add(`${colors[i]}`);
+        colorSquare.addEventListener('click', changeBrush);
         footer.appendChild(colorSquare);
     }
+
+    let currentColor = document.createElement('div');
+    currentColor.classList.add('currentColor');
+    footer.appendChild(currentColor);
 }
-createGrid(2,2);
+
+function changeBrush(event){
+    brushColor = event.target.classList.item(1);
+}
+
+createGrid(2059);
 createPalette();
