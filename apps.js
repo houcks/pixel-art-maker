24d9a8cff3773752f2dad4c9ab004c3e5a8299e1
houcks <houcks08@gmail.com>
2022-02-08 10:19:48 -0700

    fixed bug where first box would not color on mousedown

--- apps.js
+++ apps.js
@@ -1,30 +1,35 @@
 var brushColor;
 var paint = false;
+document.body.addEventListener('mousedown', function () {paint = true});
+document.body.addEventListener('mouseup', function () {paint = false});
+
 let colors = ['red', 'darkred', ,'firebrick', 'crimson', 'orangered', 'darkorange', 'orange', 'gold', 
             'lightyellow', 'moccasin', 'khaki', 'yellow', 'lawngreen', 'green', 'darkgreen', 'aquamarine', 'cyan',
             'blue', 'mediumblue', 'darkblue', 'darkviolet', 'purple', 'indigo', 'pink', 
             'hotpink', 'deeppink', 'white', 'gray', 'black'];
 
-//removes old color if not matching current brush color or paints new color
-function toggleColor(event){
-    if(paint === true){
-        if(event.target.classList.item(1) !== brushColor){
-            event.target.classList.remove(event.target.classList.item(1));
-        }
-        event.target.classList.toggle(`${brushColor}`); 
-    }
-}
-
+//creates a grid of 'square' div's and add event listeners to trigger coloring
 function createGrid(size){
     let grid = document.getElementById('grid');
         for(var j = 0; j < size; j++){
             let squares = document.createElement('div');
             squares.classList.add('square');
-            squares.addEventListener('mousedown', function () {paint = true});
-            squares.addEventListener('mouseup', function () {paint = false});
+            squares.addEventListener('mousedown', function() {paint = true; toggleColor()});
             squares.addEventListener('mouseenter', toggleColor);
             grid.appendChild(squares);
         }
+}
+
+//removes old color if not matching current brush color or paints new color
+function toggleColor(){
+    var square = document.querySelectorAll(':hover');
+    square = square[square.length - 1];
+    if(paint === true && square.classList.item(0) === 'square'){
+        if(square.classList.item(1) !== brushColor){
+            square.classList.remove(square.classList.item(1));
+        }
+        square.classList.add(`${brushColor}`); 
+    }
 }
 
 //loops over array of colors to create palette of colors to choose from
